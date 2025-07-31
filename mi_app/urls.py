0723c10c778dffc6edaab8c73726977b3ccc0ae3
Jorge <jorgehuillcacondori@gmail.com>
2025-07-31 13:43:47 -0500

    prueba de tests-todo ok

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.upload_json, name='dashboard'),  # Home page view    
+    path('', views.upload_json, name='upload_json'),  # Home page view    
 ]
